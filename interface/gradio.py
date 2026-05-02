0bb526c54475ac55f0ec2adc54f40ee1a6dca096
Hubert Hung <hubert322@gmail.com>
2026-05-02 10:47:51 -0400

    refactor: update import path for copy_state_dict from training.utils to models.utils

--- interface/gradio.py
+++ interface/gradio.py
@@ -15,9 +15,8 @@ from torchaudio import transforms as T
 from stable_audio_tools.inference.generation import generate_diffusion_cond, generate_diffusion_uncond
 from stable_audio_tools.models.factory import create_model_from_config
 from stable_audio_tools.models.pretrained import get_pretrained_model
-from stable_audio_tools.models.utils import load_ckpt_state_dict
+from stable_audio_tools.models.utils import load_ckpt_state_dict, copy_state_dict
 from stable_audio_tools.inference.utils import prepare_audio
-from stable_audio_tools.training.utils import copy_state_dict
 
 from loraw.network import LoRAMerger
 
